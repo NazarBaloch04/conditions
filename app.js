d949468d71fbfd8ef8b9263ac54c376bdd6054a1
NazarBaloch04 <153612842+NazarBaloch04@users.noreply.github.com>
2024-03-25 14:42:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+"use strict";
+//  let guess a number For grading system
+let percentage = 98;
+//  Using Conditions
+if (percentage >= 90 && percentage <= 99.99) {
+    console.log("You got A+");
+}
+else if (percentage >= 80 && percentage <= 89.99) {
+    console.log("You got A");
+}
+else if (percentage >= 70 && percentage <= 79.99) {
+    console.log("You got B");
+}
+else if (percentage >= 60 && percentage <= 69.99) {
+    console.log("You got C");
+}
+else if (percentage >= 50 && percentage <= 59.99) {
+    console.log("You got D");
+}
+else {
+    console.log("fail");
+}
